0a069c8e74f5f0f089bc0ed63a6e157be6c9c201
vagrant <vagrant@homestead>
2017-05-30 14:17:22 +0000

    trablaho 30052017

--- app/Http/Controllers/LoteriaController.php
+++ app/Http/Controllers/LoteriaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Resultado;
 
 class LoteriaController extends Controller
 {
@@ -14,8 +15,23 @@ class LoteriaController extends Controller
     }
 
     public function index(){
-    	//$ncms = Ncm::paginate(10);
     	$url = 'quina';
-        return view('loteria.index', compact('url'));
+    	for ($i=1; $i < 71; $i++) { 
+    		$n = $i;
+    		$r = $this->conta($n);
+    		echo $i.'<=>'.$r."<br>";
+     	}
+    	return view('loteria.index', compact('url', 'resultado'));
     }
+
+
+    public function conta($n){
+    	$contagem = Resultado::where('P', $n)
+    							->orWhere('S', $n)
+    							->orWhere('T', $n)
+    							->orWhere('Q', $n)
+    							->orWhere('QT', $n)->get();
+    	return count($contagem);
+    }
+
 }
